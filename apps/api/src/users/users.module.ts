5adb0903d714173a71172234b31269d3aa0337d0
Colin Espinas <colin.espinas@yahoo.fr>
2025-10-20 15:37:51 +0200

    fix: renamed prisma users repo for better convention

--- apps/api/src/users/users.module.ts
+++ apps/api/src/users/users.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { PrismaService } from 'src/prisma.service';
 import { UsersRepository } from './repositories/users.repository';
-import { PrismaUsersRepository } from './repositories/prisma-user.repository';
+import { PrismaUsersRepository } from './repositories/prisma-users.repository';
 
 @Module({
   providers: [
