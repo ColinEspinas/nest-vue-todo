7a5068652e35b9f956fcec09266eb3c48cbeea65
Colin Espinas <colin.espinas@yahoo.fr>
2025-10-22 14:05:12 +0200

    feat: better error design

--- apps/web/src/components/app/error-message.vue
+++ apps/web/src/components/app/error-message.vue
@@ -7,10 +7,10 @@ defineProps<{
 </script>
 
 <template>
-  <div class="bg-base-200 border-2 border-error rounded-lg p-4 mb-6">
-    <div class="flex items-center">
-      <Icon icon="ph:x-circle" class="w-5 h-5 text-error mr-2" />
-      <span class="text-error">{{ error }}</span>
+  <div class="bg-error/10 border-2 border-error/20 rounded-3xl p-2 mb-6">
+    <div class="flex items-center bg-base-100/80 p-4 rounded-2xl border-2 border-error/30">
+      <Icon icon="ph:warning" class="w-5 h-5 text-error mr-2" />
+      <span class="text-base-content-100">{{ error }}</span>
     </div>
   </div>
 </template>
