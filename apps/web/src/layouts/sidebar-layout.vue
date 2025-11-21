7195a393e01e0f12956b7bb9ccf775f6612eece6
Colin Espinas <colin.espinas@yahoo.fr>
2025-11-22 00:43:09 +0100

    fix: fixed messed up layout

--- apps/web/src/layouts/sidebar-layout.vue
+++ apps/web/src/layouts/sidebar-layout.vue
@@ -3,12 +3,14 @@ import AppSidebar from '@/components/app/app-sidebar.vue';
 </script>
 
 <template>
-  <div class="sticky top-0 left-1/2 -translate-x-1/2 flex h-full mx-auto max-w-[900px]">
-    <AppSidebar class="sticky left-0 top-4 h-[calc(100vh-2rem)] my-4" />
-    <div class="flex flex-col justify-between w-[600px] p-4 min-h-screen">
-      <main>
-        <RouterView />
-      </main>
+  <div class="flex justify-center">
+    <div class="flex h-full max-w-[900px]">
+      <AppSidebar class="sticky left-0 top-4 h-[calc(100vh-2rem)] my-4" />
+      <div class="flex flex-col justify-between w-[600px] p-4 min-h-screen">
+        <main>
+          <RouterView />
+        </main>
+      </div>
     </div>
   </div>
 </template>
