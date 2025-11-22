2f5876aa1f98f082949a386ac85d5ddd5c07c39d
Colin Espinas <colin.espinas@yahoo.fr>
2025-11-22 13:39:45 +0100

    fix: checkbox is now using accent color

--- apps/web/src/components/ui/ui-checkbox.vue
+++ apps/web/src/components/ui/ui-checkbox.vue
@@ -34,15 +34,15 @@ const emits = defineEmits<{
         @change="emits('change', !checked)"
       />
       <div
-        class="w-5 h-5 border-2 rounded transition-all duration-300 ease-out flex items-center justify-center flex-shrink-0 ring-0 ring-base-content-100/20 bg-base-100 border-base-300 scale-100 peer-checked:bg-base-content-100 peer-checked:border-base-content-100 peer-checked:scale-100 peer-disabled:opacity-50 peer-disabled:cursor-not-allowed peer-hover:ring-3 peer-hover:border-base-content-200 peer-disabled:hover:ring-0 peer-disabled:hover:border-base-300 peer-disabled:hover:bg-base-100 peer-checked:peer-disabled:hover:bg-base-content-100 peer-checked:peer-disabled:hover:border-base-content-100"
+        class="w-5 h-5 border-2 rounded transition-all duration-300 ease-out flex items-center justify-center flex-shrink-0 ring-0 ring-accent/20 bg-base-100 border-base-300 scale-100 peer-checked:bg-accent peer-checked:border-accent peer-checked:scale-100 peer-disabled:opacity-50 peer-disabled:cursor-not-allowed peer-hover:ring-3 peer-hover:border-accent peer-disabled:hover:ring-0 peer-disabled:hover:border-base-300 peer-disabled:hover:bg-base-100 peer-checked:peer-disabled:hover:bg-accent peer-checked:peer-disabled:hover:border-accent"
         :class="[!checked && 'peer-hover:bg-base-200']"
       >
         <Icon
           icon="ph:check-bold"
           :class="[
-            'w-3 h-3 transition-all ease-out',
+            'w-3.5 h-3.5 transition-all ease-out',
             checked
-              ? 'opacity-100 scale-100 duration-200 text-white'
+              ? 'opacity-100 scale-100 duration-200 text-base-100'
               : 'opacity-0 scale-50 duration-150 text-base-content-100',
           ]"
           :style="{
